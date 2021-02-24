291f1e986b905efdf9bffd0b09fb8213cbcc220a
Harish Anantharajah <37521868+harishanan@users.noreply.github.com>
2021-02-24 12:46:49 -0800

    jeremy-stuff

--- src/componentsv2/Homepage.js
+++ src/componentsv2/Homepage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import axios from "axios"; 
 
@@ -8,8 +8,7 @@ const headers = {
   'Host': 'localhost:3000',
   'Content-Length': '105',
   'Accept': '*/*',
-  'Cookie': '_ga=GA1.2.2116708883.1610681366; __utmz=109787653.1610751062.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=s9r60ceklpl4r1i58psbi9cav0; __utma=109787653.2116708883.1610681366.1613162109.1613681064.18; __utmb=109787653.0.10.1613681064; __utmc=109787653',
-  'strict-origin-when-cross-origin': '*'
+  'Cookie': '_ga=GA1.2.2116708883.1610681366; __utmz=109787653.1610751062.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=s9r60ceklpl4r1i58psbi9cav0; __utma=109787653.2116708883.1610681366.1613162109.1613681064.18; __utmb=109787653.0.10.1613681064; __utmc=109787653'
 }
 
 const data = {
@@ -30,11 +29,71 @@ const postQR = () => {
 
 
 function Homepage() {
+
+  const [postQrNow, setPostQrNow] = useState(false)
+
+  const postQR = () => {
+    $('#idsaretoxicareyoustupid').html(
+      <form method="POST" action="">
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+      </form> 
+    ).submit();
+
+  }
+
+
   return (
-    <div>
-      <input type="button" value="Post" onClick={postQR}/>
-    </div>
+    <>
+      <div id="idsaretoxicareyoustupid" style="visibility:hidden">
+        <img src="....."></img>
+        <script src="cdn.jquery.com/jquery.js"></script>
+        <script>
+          // nothing I can do here can see the actual fucking jquery code itself.
+          // but it CAN see any global variables that were written-to by jquery.js, because, duh, they're global variables.
+          // for example, jquery.js does, more or less, this:
+          // $ = () => { /* holy shit so much code */ }
+        </script>
+
+      </div>
+      <div>
+        <input type="button" value="Post" onClick={postQR}/>
+      </div>
+    </>
   );
+}
+
+
+
+
+// function Homepage() {
+
+//   const [postQrNow, setPostQrNow] = useState(false)
+
+//   const postQR = () => {
+
+//     // maybe also set some other state or some shit, so that the form knows what to read
+
+//     setPostQrNow(true);
+
+//   }
+
+//   if (postQrNow) {
+//     return (
+//       <form>
+//         <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+//       </form> 
+//     )
+//   } else {
+
+//     return (
+//       <div>
+//         <input type="button" value="Post" onClick={postQR}/>
+//       </div>
+//     );
+//   }
 }
 
 export default Homepage;
