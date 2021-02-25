5cbb51197d7092e7f016f2ec9c8328ddb98a6685
Shayan Sabzkoohi <shayansabzkoohi@Shayans-MacBook-Pro.local>
2021-02-25 18:16:43 +0330

    MVP last beta

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,7 +13,16 @@ function Profile(props) {
     let customer_id = parseInt(props.location.query.customer_id);
     const [customers, setCustomers] = useState([0]);
     const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
-
+    const [conName, setContactName] = useState([0]);
+    const [email, setContactEmail] = useState([0]);
+    const [phone, setContactPhone] = useState([0]);
+    const [address, setAddress] = useState([0]);
+    const [city, setCity] = useState([0]);
+    const [prov, setProv] = useState([0]);
+    const [postal, SetPostal] = useState([0]);
+    const [country, setCountry] = useState([0]);
+    const [empName, setEmpName] = useState([0]);
+    const [empEmail, setEmpEmail] = useState([0]);
     // let [org_name,address,city,prov,postalcode,country,name,email,phone] = ["","","","","","","","",""]
 
     useEffect(() => {
@@ -25,9 +34,18 @@ function Profile(props) {
       "customer_id": customer_id 
       })
     .then(function (response) {
-      setCustomers([response.data.Item])
-      // console.log(customers[0])
-
+      setCustomers([response.data.Item]);
+      console.log(response.data.Item)
+      setAddress([response.data.Item.ship_address.Address]);
+      setCity([response.data.Item.ship_address.City]);
+      setProv([response.data.Item.ship_address.prov]);
+      SetPostal([response.data.Item.ship_address.post_code]);
+      setCountry([response.data.Item.ship_address.country]);
+      setContactName([response.data.Item.contact_name]);
+      setContactEmail([response.data.Item.contact_person.email]);
+      setContactPhone([response.data.Item.contact_person.phone]);
+      setEmpName([response.data.Item.partner_contact.name])
+      setEmpEmail([response.data.Item.partner_contact.email])
         // let org_name = response.data.Item.contact_name
         // let name= response.data.Item.contact_name
         // let email = response.data.Item.contact_person.email
@@ -45,11 +63,134 @@ function Profile(props) {
     });
     const customers_list = customers.map(customer => (
       <>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-3 cus_details">
+      <div className="container-fluid mg-20">
+        <div className="row mg-20">
+          <div className="col-md-3 cus_details mg-20">
             <div className="row">
-              <div className="col">
+              <div className="col-12 text-center">
+              <p className="font-weight-bold">Customer Details</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Org. Name:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {customer.org_name ? customer.org_name : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Customer ID:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {customer.customer_id ? customer.customer_id : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-center">
+              <p className="font-weight-bold">Shipping</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Address:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {address ? address : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>City:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {city ? city : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Prov/State:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {prov ? prov : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Postal Code:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {postal ? postal : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Country</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {country ? country : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-center">
+              <p className="font-weight-bold">Contact</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Name:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {conName ? conName : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Email:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {email ? email : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Phone:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {phone ? phone : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12 text-center">
+              <p className="font-weight-bold">BuiltSpace Sales Contact</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Name:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {empName ? empName : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Emp. ID:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {customer.partner_id ? customer.partner_id : "Loading.."}
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+              <p>Email:</p>
+              </div>
+              <div className="col-md-6 text-center">
+              {empEmail ? empEmail : "Loading.."}
+              </div>
+            </div>
+              {/* <div className="col">
             <p className="font-weight-bold">Customer Details</p>
             <p>Org. Name:</p>
             <p>Customer ID:</p>
@@ -67,8 +208,8 @@ function Profile(props) {
             <p>Phone: </p>
             <p className="font-weight-bold">BuiltSpace Sales Contact</p>
             <p>Name: </p>
-            {/* <p>Emp ID: {customer.partner_id}</p>
-            <p>Email: {customer.partner_contact}</p> */}
+            <p>Emp ID: </p>
+            <p>Email: </p>
               </div>
               <div className="col">
               <p> '</p>
@@ -88,12 +229,12 @@ function Profile(props) {
               <p>PTEMP</p>
               <p className="font-weight-bold">'</p>
               <p>TEMP</p>
-              {/* <p>Emp ID: {customer.partner_id}</p>
-              <p>Email: {customer.partner_contact}</p> */}
-              </div>
-            </div>
+              <p>Emp ID:</p>
+              <p>Email:</p>
+            </div> */}
+            
           </div>
-          <div className="col-9 order_div">
+          <div className="col-md-9 order_div">
             <div className="row">
               <div className="container-fluid ord_table">
               <Order
