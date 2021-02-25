b9049359f8b05570b1565bd792b48d82bd625fb0
Shayan Sabzkoohi <shayansabzkoohi@Shayans-MacBook-Pro.local>
2021-02-25 15:59:14 +0330

    new order functionality added

--- src/components/create_order.js
+++ src/components/create_order.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import Order from './Order'
-
+import Button from 'react-bootstrap/Button'
 import awsconfig from '../aws-exports';
-
+import "../styles/create_order.css";
 
 // This component is tied with the CreateOrder component to select a customer ID then make orders for it
 
 function Customer(props) {
-    const customer_id = parseInt(props.location.query.customer_id);
+  let customer_id = "";
+  if (props.location.query){
+    customer_id = parseInt(props.location.query.customer_id);
+  } else {
+    customer_id = "Unknown";
+  }
     const [customers, setCustomers] = useState([0]);
-    const [message, setMessage] = useState('');
+    const [phone, setPhone] = useState([0]);
+    const [email, setEmail] = useState([0]);
     const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
-    const [id, setId] = useState(0);
-    const [searched, setSearch] = useState(false)
+
 
 
     useEffect(() => {
@@ -21,88 +25,163 @@ function Customer(props) {
     }, []);
 
 
+    function postOrder(e){
+      
+      e.preventDefault();
+      console.log(e)
+      console.log(e)
+      console.log(e)
+      axios.post(URL + '/orders', 
+        {
+        customer_id: customer_id,
+        num_urls: 201,
+        })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
 
 
 const customers_list = customers.map(customer => (
   <>
-<div id={customer_id} style={{padding: '1em'}}>
-<li id="customer_id">
-  <b>Customer ID:</b> {customer_id}
-</li>
-<li id="contact_name">
-  <b>Name:</b> {customer.contact_name}
-</li>
-<li id="email">
-  <b>Organization:</b>: {customer.org_name}
-</li>
+<div className="container-fluid wrapper">
+  <div className="container">
+  <form onSubmit={postOrder} className="orderForm bg-light">
+    <div className="form-row">
+      <div className="form-group col text-center">
+      <h2>Submit an Order</h2>
+      </div>
+    </div>
+      <div className="form-row">
+        <div className="form-group col">
+          <p>Customer Name:</p>
+        </div>
+        <div className="form-group col text-center">
+          <p>{customer.contact_name}</p>
+        </div>
+      </div>
+      <div className="form-row">
+      <div className="form-group col-3">
+          <p>Customer Contact:</p>
+      </div>
+      <div className="form-group col-3 text-center">
+        <p>{phone}</p>
+      </div>
+      <div className="form-group col-3 text-center">
+        <p>{email}</p>
+      </div>
+      <div className="form-group col-3 text-center">
+      </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col">
+          <p>Delivery Method:</p>
+        </div>
+        <div className="form-group col text-center">
+        <select id="inputDelivery" class="form-control" disabled>
+          <option selected>Shipping</option>
+          <option>Pick Up</option>
+        </select>
+        </div>
+        <div className="form-group col text-center">
+          <p>Quantity:</p>
+        </div>
+        <div className="form-group col text-center">
+        <input className="form-control"
+          type="text"
+          name="quantity"
+          autoComplete="off"
+          pattern="[0-9]+"
+          required>43</input>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col">
+          <p>Received Via:</p>
+        </div>
+        <div className="form-group col text-center">
+        <select id="inputReceived" class="form-control" disabled>
+          <option selected>Phone</option>
+          <option>Email</option>
+        </select>
+        </div>
+        <div className="form-group col text-center">
+          <p>QR Design:</p>
+        </div>
+        <div className="form-group col text-center">
+        <input type="text" class="form-control" id="inputQR" disabled></input>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col-12 text-center">
+
+      <textarea class="form-control" rows="3" disabled>Notes</textarea>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col text-center">
+          <p>Date Received:</p>
+        </div>
+        <div className="form-group col text-center">
+        <input type="text" class="form-control" disabled></input>
+        </div>
+        <div className="form-group col text-center">
+          <p>Received By:</p>
+        </div>
+        <div className="form-group col text-center">
+        <input type="text" class="form-control" disabled></input>
+        </div>
+        <div className="form-group col text-center">
+          <p>Employee ID:</p>
+        </div>
+        <div className="form-group col text-center">
+          <input type="text" class="form-control" disabled></input>
+        </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group col text-center">
+          <button type="submit" className="btn btn-primary btn-green">Submit</button>
+        </div>
+        <div className="form-group col text-center">
+          <button type="submit" className="btn btn-primary">Cancel</button>
+        </div>
+      </div>
+  </form>
+  </div>
 </div>
 
 </>
 ));
 
-
-      function handleSubmit() {
+  function readData() {
+    if (customer_id !== "Unknown"){
       axios.get(URL + '/customers/' + customer_id, 
         {
         "customer_id": customer_id 
         })
       .then(function (response) {
-
-          console.log(response);
-        if (Object.keys(response.data).length === 0){
-            setMessage('Customer does not exit.' )
-            setCustomers([])
-        }else {
-            setMessage('')
-            console.log(response.data)
-            setCustomers([response.data.Item])
-            setId(response.data.Item.customer_id)
-            setSearch(true);        }
+        setCustomers([response.data.Item])
+        setPhone([response.data.Item.contact_person.phone])
+        setEmail([response.data.Item.contact_person.email])
         })
       .catch(function (error) {
         console.log(error);
       });
+    } else {
+      setCustomers([{contact_name:"Unknown"}])
+      setPhone(["Unknown"])
+      setEmail(["Unknown"])
     }
-    handleSubmit()
-    return (
-
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col">
-              <h2>Customer Name</h2>
-              <p>Customer ID</p>
-              <p>Customer Contact</p>
-              <p>Delivery Method</p>
-              <p>Received via</p>
-            </div>
-            <div className="col">
-              <h2>John Smith</h2>
-              <p>A2343242</p>
-              <p>6044444444</p>
-              <p> ""DROPDOWN MENU ""</p>
-              <p> ""DROPDOWN MENU ""</p>
-            </div>
-            <div className="col">
-              <p></p>
-              <p></p>
-              <p>something@gmail.com</p>
-              <p> quantity</p>
-              <p>QR design code</p>
-            </div>
-            <div className="col">
-              <p> STATUS</p>
-              <input></input>
-              <input></input>
-            </div>
-          </div>            
-          {/* {customers_list} */}
-       
-            {/* <Order
-            customer_id={customer_id} />
-            */}
-      
-       </div>
+}
+    readData()
+    return (   
+      <>
+      {customers_list}
+      </>
     );
 }
-
+    
 export default Customer;
