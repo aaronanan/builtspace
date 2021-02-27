44c4fe54e173bc5041a43942559d9f41eaeb268b
HenrikTDang <henrikt.dang@gmail>
2021-02-27 15:37:35 -0800

    update Orders tbl from Shayan Branch

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -13,8 +13,13 @@ function Orders(props) {
     let search = ""
     const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
 
+    const [customers, setCustomers] = useState([0]);
 
     useEffect(getOrders, []);
+    useEffect(getCustomers, []);
+
+    const mergedOrdersCustomers = mergeCusOrd(customers, orders)
+    // console.log(mergedOrdersCustomers);
 
     const orders_list = (
       <>
@@ -38,24 +43,30 @@ function Orders(props) {
           <div className="col-12">
           <Table size="sm" striped bordered hover>
           <thead className="thead-green">
-            <tr>
-              <th className="text-center left_radius">Customer ID</th>
-              <th className="text-center">Status</th>
-              <th className="text-center">Amount of URLs</th>
-              <th className="text-center">Date Created</th>
-              <th className="text-center right_radius">Order ID</th>
+          <tr>
+            <th className="text-center">Order ID</th>
+            <th className="text-center">Customer ID</th>
+            <th className="text-center">Organization</th>
+            <th className="text-center"># URLs</th>
+            <th className="text-center">QR Design Code</th>
+            <th className="text-center">Status</th>
+            <th className="text-center">Date Created</th>
+            <th className="text-center">Date Updated</th>
             </tr>
           </thead>
           <tbody>
-          {orders.map(order =>
+          {mergedOrdersCustomers.map(order =>
         
-              <tr>
-                <td className="text-center">{String(order.customer_id).padStart(4, '0')}</td>
-                <td className="text-center">{order.status}</td>
-                <td className="text-center">{order.num_urls}</td>
-                <td className="text-center">{String(order.creation_date).slice(0, 10)}</td>
-                <td className="text-center" id="order_id">{order.order_id}</td>
-              </tr>
+            <tr>
+              <td className="text-center">{order.order_id}</td>
+              <td className="text-center">{String(order.customer_id).padStart(4, '0')}</td>
+              <td className="text-center">{order.org_name}</td>    
+              <td className="text-center">{order.num_urls}</td>
+              <td className="text-center">Link to DB</td>
+              <td className="text-center">{order.status}</td>
+              <td className="text-center">{String(order.creation_date).slice(0, 10)}</td>
+              <td className="text-center">{String(order.lastupdate_date).slice(0, 10)}</td>
+            </tr>
           )}
           </tbody>
           </Table>
@@ -69,6 +80,37 @@ function Orders(props) {
       </div>
     </>
     );
+
+    function getCustomers(ord) {
+      axios.get(URL + '/customers')
+      .then(function (response) {
+        // console.log(response.data.Items);
+        const newCustomers = response.data.Items
+        setCustomers(newCustomers);
+
+        return response.data.Items
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    };
+    
+    
+    function mergeCusOrd(cus, ord) {
+      let customerIDs = cus.map((cusItem) => {
+        return cusItem['customer_id']
+      })
+      let mergedListOfDict = []
+      for (let orderItem of ord) {
+        if (customerIDs.includes(orderItem.customer_id)) {
+          let customerDict = cus[customerIDs.indexOf(orderItem.customer_id)]
+          let ordCusDict = Object.assign({}, orderItem, customerDict)
+          mergedListOfDict.push(ordCusDict)
+        }
+      }
+      return mergedListOfDict
+    }
+
 
     function getOrders() {
         axios.get(URL + '/orders')
@@ -97,10 +139,10 @@ function Orders(props) {
           console.log(error);
         });
     };
-     function handleChange(e){
-       search = e.target.value
-       getOrders()
-     }
+    function handleChange(e){
+      search = e.target.value
+      getOrders()
+    }
   return (
       <div>
         {orders_list}
