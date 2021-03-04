ac799ef08952e79ec4fd9cf31ae25734f0bf1d93
Harish Anantharajah <37521868+harishanan@users.noreply.github.com>
2021-03-04 14:37:48 -0800

    working post

--- src/components/QRGen.js
+++ src/components/QRGen.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { CSVLink, CSVDownload } from "react-csv";
+// import React, { Component } from 'react';
+// import { CSVLink, CSVDownload } from "react-csv";
 
-function QRGen() {
+// function QRGen() {
 
-    const csvData = [
-        ["firstname", "lastname", "email"],
-        ["Ahmed", "Tomi", "ah@smthing.co.com"],
-        ["Raed", "Labes", "rl@smthing.co.com"],
-        ["Yezzi", "Min l3b", "ymin@cocococo.com"]
-      ];
+//     const csvData = [
+//         ["firstname", "lastname", "email"],
+//         ["Ahmed", "Tomi", "ah@smthing.co.com"],
+//         ["Raed", "Labes", "rl@smthing.co.com"],
+//         ["Yezzi", "Min l3b", "ymin@cocococo.com"]
+//       ];
 
-        return (
-            <div>
-                Hello Test 
-                <CSVLink data={csvData}>Download me</CSVLink>;
-            </div>
-        );
-}
+//         return (
+//             <div>
+//                 Hello Test 
+//                 <CSVLink data={csvData}>Download me</CSVLink>;
+//             </div>
+//         );
+// }
 
-export default QRGen;
+// export default QRGen;
--- src/componentsv2/Homepage.js
+++ src/componentsv2/Homepage.js
@@ -2,31 +2,31 @@ import React, { Component, useState } from "react";
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import axios from "axios"; 
 
-const headers = {
-  'Content-Type': 'application/x-www-form-urlencoded',
-  'Connection': 'keep-alive',
-  'Host': 'localhost:3000',
-  'Content-Length': '105',
-  'Accept': '*/*',
-  'Cookie': '_ga=GA1.2.2116708883.1610681366; __utmz=109787653.1610751062.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=s9r60ceklpl4r1i58psbi9cav0; __utma=109787653.2116708883.1610681366.1613162109.1613681064.18; __utmb=109787653.0.10.1613681064; __utmc=109787653'
-}
+// const headers = {
+//   'Content-Type': 'application/x-www-form-urlencoded',
+//   'Connection': 'keep-alive',
+//   'Host': 'localhost:3000',
+//   'Content-Length': '105',
+//   'Accept': '*/*',
+//   'Cookie': '_ga=GA1.2.2116708883.1610681366; __utmz=109787653.1610751062.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); PHPSESSID=s9r60ceklpl4r1i58psbi9cav0; __utma=109787653.2116708883.1610681366.1613162109.1613681064.18; __utmb=109787653.0.10.1613681064; __utmc=109787653'
+// }
 
-const data = {
-  'inputTextValues2': '[["www.google.ca"],["www.test.com"],[null]]',
-  'outputFolderName': 'test222'
-}
+// const data = {
+//   'inputTextValues2': '[["www.google.ca"],["www.test.com"],[null]]',
+//   'outputFolderName': 'test222'
+// }
 
-const postQR = () => {
-  fetch("https://qed.builtspace.com/webapp_Builtspace/upload_manual2.php", {
-  method: 'POST',
-  headers: headers,
-  body: data
-}).then(res => {
-  return res
-})
-.then(data => console.log(data))
-.catch(error => console.log('ERROR'))
-}
+// const postQR = () => {
+//   fetch("https://qed.builtspace.com/webapp_Builtspace/upload_manual2.php", {
+//   method: 'POST',
+//   headers: headers,
+//   body: data
+// }).then(res => {
+//   return res
+// })
+// .then(data => console.log(data))
+// .catch(error => console.log('ERROR'))
+// }
 
 
 
@@ -36,17 +36,17 @@ function Homepage() {
 
   // const [postQrNow, setPostQrNow] = useState(false)
 
-  // const postQR = () => {
-  //   $('#idsaretoxicareyoustupid').html(
-  //     <form method="POST" action="">
-  //       <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
-  //       <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
-  //       <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
-  //       <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
-  //     </form> 
-  //   ).submit();
+  const postQR = () => {
+    // $('#idsaretoxicareyoustupid').html(
+      <form method="POST" action="https://qed.builtspace.com/webapp_Builtspace/upload_manual2.php" target="_blank">
+        <input type="text" name="outputFolderName" id="inputTextValues2"></input>
+        {/* <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input>
+        <input type="hidden" name="stuffstuff" value="allthiscrapomgsomuchdata"></input> */}
+      </form> 
+    // ).submit();
 
-  // }
+  }
 
 
   return (
@@ -63,7 +63,20 @@ function Homepage() {
 
       {/* </div> */}
       <div>
-        <input type="button" value="Post" onClick={postQR}/>
+        {/* <form method="POST" action="https://qed.builtspace.com/webapp_Builtspace/upload_manual2.php" target="_blank"> */}
+        <form method="POST" action="http://ec2-34-212-38-72.us-west-2.compute.amazonaws.com/upload_manual2.php" target="_blank">
+        <input type="text" name="outputFolderName" id="inputTextValues2"></input>
+        <input type="text" name="inputTextValues2" id="inputTextValues2"></input>
+        {/* [[www.bcit.ca],[null]] */}
+        <input type="submit" value="Submit"></input>
+      </form>
+      <div>
+      <form method="POST" action="http://ec2-34-212-38-72.us-west-2.compute.amazonaws.com/users_db.php" target="_blank">
+        <input type="text" id="username" name="username" placeholder="Email" value="alenbakovic@hotmail.ca"/>
+        <input type="password" id="password" name="password" placeholder="Password" value="Built.space123"></input>
+        <input type="submit" value="Submit" onClick={window.open('http://www.google.com')}></input>
+      </form>
+      </div>  
       </div>
     </>
   );
