cf82a96f656bd96e4f8991e85b7d4ce94fcd9495
jsw13 <joseph.sze.wong@gmail.com>
2021-03-05 10:58:22 -0800

    fixed profile page making continuous get requests

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,59 +1,59 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 import awsconfig from '../aws-exports';
 import CopyToClipboard from 'react-copy-to-clipboard';
 // TODO: Integrate unused Modal code to display URLs for a specific order, requires new //GET /orders/order_id endpoint
 
+const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
+
 function Order(props) {
 
-    const [orders, setOrders] = useState([0]);
-    const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
+  const [orders, setOrders] = useState([0]);
 
+  useEffect(getSpecificOrders, [])
 
-    const orders_list = (
-      <table className="table table-bordered table-sm  hover">
-        <thead className="thead-green">
-          <tr>
-            <th className="text-center left_radius">Order ID</th>
-            <th className="text-center">Status</th>
-            <th className="text-center">Amount</th>
-            <th className="text-center">Date Created</th>
-            <th className="text-center right_radius">URL LIST</th>
-          </tr>
-        </thead>
-        <tbody>
-      {orders.map(order =>
+  function getSpecificOrders() {
+    axios.get(URL + '/orders/' + props.customer_id)
+    .then(function (response) {
+      const newOrders = response.data
+      var i;
+      for (i = 0; i < newOrders.length; i++) {
+        // const pad_id = String(newOrders[i].customer_id)
+        newOrders[i].customer_id = String(newOrders[i].customer_id).padStart(4, '0')
+      } 
+      setOrders(newOrders);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  const orders_list = (
+    <table className="table table-bordered table-sm  hover">
+      <thead className="thead-green">
         <tr>
-          <td className="text-center">{order.order_id}</td>
-          <td className="text-center">{order.status}</td>
-          <td className="text-center">{order.num_urls}</td>
-          <td className="text-center">{String(order.creation_date).slice(0, 10)}</td>
-          <td className="text-center"><CopyToClipboard text={order.urls ? order.urls.join("\n") : ""}><a className="btn btn-primary btn-theme">Copy URLs to Clipboard</a></CopyToClipboard></td>
+          <th className="text-center left_radius">Order ID</th>
+          <th className="text-center">Status</th>
+          <th className="text-center">Amount</th>
+          <th className="text-center">Date Created</th>
+          <th className="text-center right_radius">URL LIST</th>
         </tr>
-      )}
+      </thead>
+      <tbody>
+        {orders.map(order =>
+          <tr>
+            <td className="text-center">{order.order_id}</td>
+            <td className="text-center">{order.status}</td>
+            <td className="text-center">{order.num_urls}</td>
+            <td className="text-center">{String(order.creation_date).slice(0, 10)}</td>
+            <td className="text-center"><CopyToClipboard text={order.urls ? order.urls.join("\n") : ""}><a className="btn btn-primary btn-theme">Copy URLs to Clipboard</a></CopyToClipboard></td>
+          </tr>
+        )}
       </tbody>
     </table>
     );
-    
-    function getSpecificOrders() {
-        axios.get(URL + '/orders/' + props.customer_id)
-        .then(function (response) {
-          const newOrders = response.data
-          var i;
-          for (i = 0; i < newOrders.length; i++) {
-                const pad_id = String(newOrders[i].customer_id)
-                newOrders[i].customer_id = pad_id.padStart(4, '0')
-          } 
-          setOrders(newOrders);
-          return response.data
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
 
-    getSpecificOrders()
   return (
         <ul>
             {orders_list}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,36 +5,36 @@ import awsconfig from '../aws-exports';
 import { Link } from "react-router-dom";
 // import "../styles/Profile.css";
 
-
+const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
 
 // TODO: Add more customer info fields, add axios.post request to update customer info
 
 function Profile(props) {
-    let customer_id = parseInt(props.location.query.customer_id);
-    const [customers, setCustomers] = useState([0]);
-    const URL = awsconfig.aws_cloud_logic_custom[0].endpoint;
-    const [conName, setContactName] = useState([0]);
-    const [email, setContactEmail] = useState([0]);
-    const [phone, setContactPhone] = useState([0]);
-    const [address, setAddress] = useState([0]);
-    const [city, setCity] = useState([0]);
-    const [prov, setProv] = useState([0]);
-    const [postal, SetPostal] = useState([0]);
-    const [country, setCountry] = useState([0]);
-    const [empName, setEmpName] = useState([0]);
-    const [empEmail, setEmpEmail] = useState([0]);
-    // let [org_name,address,city,prov,postalcode,country,name,email,phone] = ["","","","","","","","",""]
 
-    useEffect(() => {
-      setCustomers([0]);
-    }, []);
+  let customer_id = parseInt(props.location.query.customer_id);
+  const [customers, setCustomers] = useState([0]);
+  const [conName, setContactName] = useState([0]);
+  const [email, setContactEmail] = useState([0]);
+  const [phone, setContactPhone] = useState([0]);
+  const [address, setAddress] = useState([0]);
+  const [city, setCity] = useState([0]);
+  const [prov, setProv] = useState([0]);
+  const [postal, SetPostal] = useState([0]);
+  const [country, setCountry] = useState([0]);
+  const [empName, setEmpName] = useState([0]);
+  const [empEmail, setEmpEmail] = useState([0]);
+  // let [org_name,address,city,prov,postalcode,country,name,email,phone] = ["","","","","","","","",""]
 
+  useEffect(getCustomers, [])
+
+  function getCustomers () {  
     axios.get(URL + '/customers/' + customer_id, 
       {
       "customer_id": customer_id 
       })
     .then(function (response) {
-      setCustomers([response.data.Item]);
+      const dbCustomer = response.data.Item
+      setCustomers([dbCustomer]);
       console.log(response.data.Item)
       setAddress([response.data.Item.ship_address.Address]);
       setCity([response.data.Item.ship_address.City]);
@@ -55,141 +55,142 @@ function Profile(props) {
         // let country = response.data.Item.ship_address.country
         // let postalcode = response.data.Item.ship_address.post_code
         // let prov = response.data.Item.ship_address.prov
-        
       }
       )
     .catch(function (error) {
       console.log(error);
     });
-    const customers_list = customers.map(customer => (
-      <>
-      <div className="container-fluid mg-20">
-        <div className="row mg-20">
-          <div className="col-md-3 cus_details mg-20">
-            <div className="row">
-              <div className="col-12 text-center">
+  }
+
+  const customers_list = customers.map(customer => (
+    <>
+    <div className="container-fluid mg-20">
+      <div className="row mg-20">
+        <div className="col-md-3 cus_details mg-20">
+          <div className="row">
+            <div className="col-12 text-center">
               <p className="font-weight-bold">Customer Details</p>
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Org. Name:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {customer.org_name ? customer.org_name : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Customer ID:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {customer.customer_id ? customer.customer_id : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-12 text-center">
+          </div>
+          <div className="row">
+            <div className="col-12 text-center">
               <p className="font-weight-bold">Shipping</p>
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Address:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {address ? address : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>City:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {city ? city : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Prov/State:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {prov ? prov : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Postal Code:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            </div>
+            <div className="col-md-6 text-center">
               {postal ? postal : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Country</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {country ? country : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12 text-center">
-              <p className="font-weight-bold">Contact</p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Name:</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {conName ? conName : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Country</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {country ? country : "Loading.."}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 text-center">
+            <p className="font-weight-bold">Contact</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Name:</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {conName ? conName : "Loading.."}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Email:</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {email ? email : "Loading.."}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Phone:</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {phone ? phone : "Loading.."}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 text-center">
+            <p className="font-weight-bold">BuiltSpace Sales Contact</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Name:</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {empName ? empName : "Loading.."}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <p>Emp. ID:</p>
+          </div>
+          <div className="col-md-6 text-center">
+            {customer.partner_id ? customer.partner_id : "Loading.."}
+          </div>
+        </div>
+          <div className="row">
+            <div className="col-md-6">
               <p>Email:</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {email ? email : "Loading.."}
-              </div>
             </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Phone:</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {phone ? phone : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-12 text-center">
-              <p className="font-weight-bold">BuiltSpace Sales Contact</p>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Name:</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {empName ? empName : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Emp. ID:</p>
-              </div>
-              <div className="col-md-6 text-center">
-              {customer.partner_id ? customer.partner_id : "Loading.."}
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-              <p>Email:</p>
-              </div>
-              <div className="col-md-6 text-center">
+            <div className="col-md-6 text-center">
               {empEmail ? empEmail : "Loading.."}
-              </div>
             </div>
+          </div>
               {/* <div className="col">
             <p className="font-weight-bold">Customer Details</p>
             <p>Org. Name:</p>
@@ -237,8 +238,7 @@ function Profile(props) {
           <div className="col-md-9 order_div">
             <div className="row">
               <div className="container-fluid ord_table">
-              <Order
-              customer_id={customer_id} />
+                <Order customer_id={customer_id} />
               </div>
             </div>
             <div className="row">
@@ -249,15 +249,14 @@ function Profile(props) {
                 query: { customer_id: `${customer.customer_id}` }
               }} className="btn btn-md btn-primary btn-profile">New Order</Link>
               </div>
-              <div className="col"></div>
-            </div>
+            <div className="col"></div>
           </div>
         </div>
-
       </div>
-    
+    </div>
     </>
     ));
+
   return (
       <>
       {customers_list}
